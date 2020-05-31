addc062b2be996041390575e144bee3bef0b3e45
Bfili <FilipBochniak2012@gmail.com>
2020-05-31 16:53:53 +0200

    First changes, project opening
    Code structure may be a little off due to laptop screen malfunction.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,28 @@
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!" << std::endl;
-    std::cout << "zobaczmy czy dziala" << std::endl;
+    sf::RenderWindow window(sf::VideoMode(800,1000), "Ships Game");
+    window.setFramerateLimit(60);
+    //test
+    //test
+    //test
+    sf::Event event;
+    while(window.isOpen())
+    {
+        window.clear(sf::Color::Black);
+        while(window.pollEvent(event))
+        {
+            if(event.type == sf::Event::Closed)
+            {
+                window.close();
+                std::cout << "okno zostalo zamkniete pomyslnie" << std::endl;
+            }
+        }
+        window.display();
+    }
+
     return 0;
 }
